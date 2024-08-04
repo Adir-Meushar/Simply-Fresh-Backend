14cd77e30a1601dbd7a804e7d3adfb88fa7263e3
Adir-Meushar <adirmeushar54@gmail.com>
2024-08-04 11:27:17 +0300

    added

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ require('./handlers/initialData/initialDataService');
 
 // Serve index.html for client-side routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'simply-fresh-frontend', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'Simply Fresh-frontend', 'public', 'index.html'));
 });
 
 // Handle 404 errors for API routes
